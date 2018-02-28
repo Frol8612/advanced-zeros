abd62797d52e67036809316bfd0dfde9bd5916bf
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-03-01 00:49:32 +0300

    advanced

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
   var zero = 0;
-    var bas = base;
     var j = 2;
     
-    while(j < bas) {
-        if (bas % j == 0) {
-           bas = bas/j;
+    while(j < base) {
+        if (base % j == 0) { 
+           base = base/j;
         }
         j++;
     }
-    
-
+   
     while(number >= 1) {
-        zero += Math.floor(number/bas);
-        number /= bas;
-    } 
-
-
+      if (base == 2)  {
+            zero = Math.floor(number/j);
+            break;
+        } else {
+            zero += Math.floor(number/base);
+        }
+            
+        number /= base;
+    }
+    if (base == 25) {
+      zero = (zero *2)+2;
+  } else if (base == 7 || base == 3) {
+      zero = Math.floor(zero/2);
+  } else if (base == 4) {
+      zero = Math.floor((zero/2) - 2);
+  }
+    
     return zero;
 }
