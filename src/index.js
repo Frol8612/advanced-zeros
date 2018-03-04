611a564e6461a18d7dde5914131c2cdfb3aec754
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-03-04 12:35:07 +0300

    advanced

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
   var zero = 0,
-  j = 2,
-  pow = base,
-  num = number,
-  z = 0,
-  arr = [],
-  res = {},
-  zeroArr = [],
-  keyArr = [];
+      j = 2,
+      pow = base,
+      num = number,
+      z = 0,
+      arr = [],
+      res = {},
+      zeroArr = [],
+      keyArr = [];
 
-while(j <= pow) {
-if (pow % j == 0) { 
-  pow = pow / j; 
-  arr.push(j);
-  j = 2;
-  continue;
-}
-  j++;
-}
+  while(j <= pow) {
+  if (pow % j == 0) { 
+    pow = pow / j; 
+    arr.push(j);
+    j = 2;
+    continue;
+  }
+    j++;
+  }
 
-for (var i = 0; i < arr.length; ++i)  {
-var a = arr[i];
-if (res[a] != undefined)
-    ++res[a];
-else
-    res[a] = 1;
-}
+  for (var i = 0; i < arr.length; ++i)  {
+    var a = arr[i];
+    if (res[a] != undefined)
+      ++res[a];
+    else
+      res[a] = 1;
+  } 
 
-for (const key in res) {
-keyArr.push(key);
-while (num > 1) {
-  zero += Math.floor(num/keyArr[z]);
- num /= keyArr[z];  
-}    
+  for (var key in res) {
+    keyArr.push(key);
+    while (num > 1) {
+      zero += Math.floor(num / keyArr[z]);
+      num /= keyArr[z];  
+    }    
 
-if (z != z.length -1) {
-  num = number;
-  z++;
-}
+    if (z != z.length -1) {
+      num = number;
+      z++;
+    }
 
-zeroArr.push(Math.floor(zero / res[key]));
-zero = 0;
-}
+    zeroArr.push(Math.floor(zero / res[key]));
+    zero = 0;
+  }
    
 
-return Math.min.apply(null, zeroArr);
+  return Math.min.apply(null, zeroArr);
 }
